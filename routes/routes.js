7319b74677e9947e0f7ce1524d9e3da16ef73e12
Luis Miguel Antonio Razon <luismiguelrazon1121@gmail.com>
2024-03-25 23:29:24 +0800

    functioning sql statements

--- routes/routes.js
+++ routes/routes.js
@@ -3,49 +3,99 @@ const mysql = require('mysql');
 //const web = require("../controllers/web.controller.js")
 
 var con = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
+    host: '192.168.68.108',
+    user: 'luis',
     password: 'aristeo0',
     database: 'mco2database_vismin'
 });
 
-con.connect(function(err){
-    if(err) throw err;
-    console.info("Connection Successful!");
-});
+
+function query(body){
+    var result = con.connect(function(err){
+        if(err) throw err;
+        console.info("Connection Successful!");
+
+        var result = con.query('INSERT INTO appointments VALUES (replace(uuid(),"-",""), "Queued", NOW(), "Test", NULL, NULL, "test", NULL, NULL, "Mindanao", NULL, NULL, NULL)', function(err, result) {
+            if(err) console.error('error connecting:' + err.stack);
+            console.info('insert successful');
+            return result;
+        });
+
+        con.end();
+        return "success"
+    });
+
+    return result;
+}
+
+function query_update_one(body){
+    var result = con.connect(function(err){
+        if (err) throw err;
+        console.info("Connection Successful!");
+
+        //insert query here
+        
+    })
+}
+
+function query_select(body, callback){
+
+    var output = undefined;
+    let result2 = con.connect(function(err){
+        if(err){
+            callback(err, null);
+        }
+        console.info("Connection Successful!");
+
+        let result1 = con.query('SELECT * FROM appointments LIMIT 10', function(err, result, fields) {
+            if(err) {
+                callback(err, null);
+            }
+            console.info('successful');
+            callback(null, result);
+        });
+        con.end();
+    });
+
+    console.info(output)
+    return output;
+}
+
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.redirect('/insert');
-   //res.redirect("/en/intro");
 });
+
 router.get('/insert', (req,res) => {
     res.render('insert');
 });
 
+
 router.post('/insert', (req,res) => {
     //the 32bit unique identifiers used by the system is done by unhex(replace(uuid(),'-',''))
     let body = req.body;
-    body.aptid = apptid
+    
     for (let field in body) {
         if (body[field] == "") {
             body[field] = null;
         }
     }
+
     if (body["isvirtual"] == undefined){
         body.isvirtual = false;
     } else {
         body.isvirtual = true;
     }
-    var sql = `INSERT INTO appointments VALUES (UUID(), "Queued", NOW(), ${body.queuedate}, NULL, NULL, ${body.type}, NULL, NULL, "Mindanao", NULL, NULL, NULL)`;
-    console.info(sql);
-    con.query(sql, function(err, result) {
-        if(err) throw err;
+
+    query_select(body, function(err, result)
+    {
+        console.log("Nasa labas na ako!");
         res.send(result);
-    })
-    res.send(body)
+    });
 })
+
 
 //TODO: add error handling
 module.exports = router;
