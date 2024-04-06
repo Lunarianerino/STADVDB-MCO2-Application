69d8155e789a2e5d802649e2743df99c7f405326
Lunarianerino <71534058+Lunarianerino@users.noreply.github.com>
2024-04-06 21:25:31 +0800

    feat: counter

--- routes/routes.js
+++ routes/routes.js
@@ -40,7 +40,7 @@ router.get('/', (req, res) => {
         if (err) {
             console.log(err);
         } else {
-        res.render('index', {contents: result});
+            res.render('index', {contents: result});
         }
     });
     
