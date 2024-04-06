179c840c143e1577cef2ccefe451f9f335e185aa
Lunarianerino <71534058+Lunarianerino@users.noreply.github.com>
2024-04-06 20:16:45 +0800

    feat: broken everything

--- routes/routes.js
+++ routes/routes.js
@@ -2,13 +2,23 @@ const express = require('express');
 const mysql = require('mysql');
 //const web = require("../controllers/web.controller.js")
 
+// var con = mysql.createConnection({
+//     host: '192.168.68.108',
+//     user: 'luis',
+//     password: 'aristeo0',
+//     database: 'mco2database_vismin'
+// });
+
 var con = mysql.createConnection({
-    host: '192.168.68.108',
-    user: 'luis',
+    host: 'localhost',
+    user: 'root',
     password: 'aristeo0',
     database: 'mco2database_vismin'
 });
-
+con.connect(function(err){ 
+    if(err) throw err;
+    console.info("Connection Successful!");
+});
 
 function query(body){
     var result = con.connect(function(err){
@@ -38,38 +48,28 @@ function query_update_one(body){
     })
 }
 
-function query_select(body, callback){
+function query_select(body){
 
-    var output = undefined;
-    let result2 = con.connect(function(err){
-        if(err){
+    let result = con.query(`SELECT * FROM appointments LIMIT 10`, function(err, result, fields) {
+        if(err) {
             callback(err, null);
         }
-        console.info("Connection Successful!");
-
-        let result1 = con.query('SELECT * FROM appointments LIMIT 10', function(err, result, fields) {
-            if(err) {
-                callback(err, null);
-            }
-            console.info('successful');
-            callback(null, result);
-        });
-        con.end();
+        return result;
     });
 
-    console.info(output)
-    return output;
+    return result;
 }
 
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.redirect('/insert');
+    let result = query_select({});
+    res.render('index', {contents: result});
 });
 
 router.get('/insert', (req,res) => {
-    res.render('insert');
+    res.render('partials/insert');
 });
 
 
@@ -96,6 +96,20 @@ router.post('/insert', (req,res) => {
     });
 })
 
+router.get('/find', (req,res) => {
+    res.render('partials/find');
+});
+
+router.get('/update/:query', (req,res) => {
+    let body = {};
+    body.query = req.params.query;
+
+    query_select(body, function(err, result)
+    {
+        console.log(result)
+        res.render('partials/update', {contents: result});
+    });
+});
 
 //TODO: add error handling
 module.exports = router;
